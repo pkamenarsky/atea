0e16e412973dbc7642ebecb4b4f85fe6901f1476
phil <phil@phils-Mac-Pro.local>
2012-02-16 20:56:11 +0100

    swt experiments

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject tracker "1.0.0-SNAPSHOT"
             :description "Minimalistic MacOs time tracker"
-            :dependencies [[org.clojure/clojure "1.3.0"]]
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [org.eclipse/swt-cocoa-macosx-x86_64 "3.5.2"]]
             :dev-dependencies [[vimclojure/server "2.3.1" :exclusions [org.clojure/clojure]]]
             :main hs.base.core)
