d9261bf23f313ea81fed17c0bc9f7ff45bc2fbc9
Philip Kamenarsky <pk@harmonious.at>
2012-02-20 04:12:51 +0100

    removed jdic-macos-tray from project in favor of clojars

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject tracker "1.0.0-SNAPSHOT"
             :description "Minimalistic MacOs time tracker"
             :dependencies [[org.clojure/clojure "1.3.0"]]
-            :dev-dependencies [[vimclojure/server "2.3.1" :exclusions [org.clojure/clojure]]]
+			:native-dependencies [[org.clojars.pka/jdic-macos-tray "0.0.1"]]
+            :dev-dependencies
+				[[native-deps "1.0.5"]
+				[vimclojure/server "2.3.1" :exclusions [org.clojure/clojure]]]
             :main hs.base.core)
